7d7b6430b9bd6c3d7fda047269bed47cdd0d5c50
Gustav Beije <gustavbeije@gmail.com>
2019-03-14 22:04:13 +0100

    Updates

---
+++ precache-manifest.d4ffd643c18619ea5282fbde6a828399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfc55fc6d59ccf0410f7",
+    "url": "/static/js/main.e396b926.chunk.js"
+  },
+  {
+    "revision": "5464ee75a21e37a0f4b3",
+    "url": "/static/js/2.98ea75d8.chunk.js"
+  },
+  {
+    "revision": "cfc55fc6d59ccf0410f7",
+    "url": "/static/css/main.cd65df6e.chunk.css"
+  },
+  {
+    "revision": "2bf7aa1f4537ff92cb0970ca69786401",
+    "url": "/index.html"
+  }
+];
